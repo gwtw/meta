ab944ea30876c8aa9741e4dac0ebe67e4c15438b
Daniel Imms <daimms@microsoft.com>
2018-01-19 20:00:24 -0800

    Add all the repos

--- generate-readme.js
+++ generate-readme.js
@@ -1,25 +1,54 @@
 var fs = require('fs');
 
-var repos = [
-    'ts-fibonacci-heap'
-];
+var repos = {
+    'Data structures': [
+        'csharp-data-structures',
+        'growing-with-the-web',
+        'js-data-structures',
+        'js-avl-tree',
+        'js-binary-heap',
+        'js-binomial-heap',
+        'js-fibonacci-heap',
+        'js-heap-tests',
+        'js-splay-tree',
+        'py-data-structures',
+        'ts-fibonacci-heap',
+    ],
+    'Sorting': [
+        'csharp-sorting',
+        'java-sorting',
+        'js-sorting',
+        'py-sorting',
+        'ruby-sorting',
+    ],
+    'Interview questions': [
+        'js-interview-questions',
+    ],
+    'Design patterns': [
+        'java-design-patterns',
+        'js-design-patterns',
+    ]
+};
 
 var markdown =
-    '## gwtw/meta\n' +
-    '\n' +
-    'This is a meta repo which is used as a dashboard for the repositories within the [gwtw](https://github.com/gwtw) organization.\n' +
-    '\n' +
-    '### Data structures\n' +
-    '\n' +
-    '| Repo | Build status | Coverage | &nbsp;\n' +
-    '|---|---|---|---\n';
+    `## gwtw/meta\n` +
+    `\n` +
+    `This is a meta repo which is used as a dashboard for the repositories within the [gwtw](https://github.com/gwtw) organization.\n`;
 
-repos.forEach(r => {
+for (var category in repos) {
     markdown +=
-        `| [${r}](https://github.com/gwtw/${r}) ` +
-        `| [![Build Status](https://api.travis-ci.org/gwtw/${r}.svg?branch=master)](http://travis-ci.org/gwtw/${r}) ` +
-        `| [![Coverage Status](https://coveralls.io/repos/github/gwtw/${r}/badge.svg?branch=master)](https://coveralls.io/github/gwtw/${r}?branch=master) ` +
-        `| [Issues](https://github.com/gwtw/${r}/issues)\n`;
-});
+        `\n` +
+        `### ${category}\n` +
+        `\n` +
+        `| Repo | Build status | Coverage | &nbsp;\n` +
+        `|---|---|---|---\n`;
+    repos[category].forEach(r => {
+        markdown +=
+            `| [${r}](https://github.com/gwtw/${r}) ` +
+            `| [![Build Status](https://api.travis-ci.org/gwtw/${r}.svg?branch=master)](http://travis-ci.org/gwtw/${r}) ` +
+            `| [![Coverage Status](https://coveralls.io/repos/github/gwtw/${r}/badge.svg?branch=master)](https://coveralls.io/github/gwtw/${r}?branch=master) ` +
+            `| [Issues](https://github.com/gwtw/${r}/issues)\n`;
+    });
+}
 
 fs.writeFileSync('./README.md', markdown);
